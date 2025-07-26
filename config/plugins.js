8c8326138161a04b1cd3a5dc8b5f3d7ca4515e84
Cloudberry <32018359+CloudberryLLC@users.noreply.github.com>
2025-07-27 00:38:07 +0900

    Update plugins.js

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,9 @@
-module.exports = () => ({});
+module.exports = () => ({
+  i18n: {
+    enabled: true,
+    config: {
+      defaultLocale: 'ja',
+      locales: ['ja', 'en', 'zh-Hans', 'zh-Hant'],
+    },
+  },
+});
